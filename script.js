acb1688f3056ed64832e52c158f139be4b9081ee
Siddharth <sahusidd715@gmail.com>
2024-07-25 20:56:59 +0530

    new file:   Hey.txt 	new file:   index.html 	new file:   maths.js 	new file:   package.json 	modified:   script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello world")
+//Working with node and npm
+const fs = require('node:fs');
+// console.log(fs)
+fs.appendFile("Hey.txt","Hello this a file",(err)=>{
+    console.log(err)
+})
+let fun=(val)=>{
+    console.log(val)
+}
+fun(3)
+const m=require("./maths")
+console.log(m)
