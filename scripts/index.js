b5b99139e90d8e3df90d208e70410aac29c93660
Siddharth <sahusidd715@gmail.com>
2024-07-27 07:29:33 +0530

    modified:   scripts/index.js 	modified:   style.css

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,2 @@
-document.querySelector('#btn').addEventListener('click', () => {
-    const name = document.querySelector('#name').value;
-    document.querySelector('#display_name').innerHTML = `<h2>${name}<h2>`;})
+displat_text=""
+let 
