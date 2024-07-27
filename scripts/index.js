873d217fada3b64868a78309240e8f55440082f6
Siddharth <sahusidd715@gmail.com>
2024-07-27 08:03:49 +0530

    modified:   scripts/index.js 	modified:   style.css

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,7 @@
-displat_text=""
-let 
+let display_text=""
+let display_area=document.querySelector(".display")
+let btn=document.getElementsByTagName("button")
+btn.addEventListener('click',()=>{
+    display_text+=btn.innerText;
+    display_area.innerHTML=`<p>${display_text}</p>`
+})
