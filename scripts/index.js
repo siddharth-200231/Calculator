8eff59f2d1e73ad3ceb25d7e681a48bbc8fae97c
Siddharth <sahusidd715@gmail.com>
2024-07-27 08:40:05 +0530

    modified:   scripts/index.js 	modified:   style.css

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,21 @@
-let display_text=""
-let display_area=document.querySelector(".display")
-let btn=document.getElementsByTagName("button")
-btn.addEventListener('click',()=>{
-    display_text+=btn.innerText;
-    display_area.innerHTML=`<p>${display_text}</p>`
+let display_text = "";
+let display_area = document.querySelector(".display");
+let buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(button=>{
+    button.addEventListener("click",()=>{
+        if(button.innerText=="C"){
+            display_text="";
+            display_area.innerText=display_text
+
+        }
+        else if(button.innerText!="="){
+        display_text+=button.innerText
+        display_area.innerText=display_text
+        }
+        else if(button.innerText=="="){
+            display_area.innerText=eval(display_text)
+        }
+       
+    })
 })
