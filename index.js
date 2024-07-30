326ba6fb1612581e2b3e0fe6fe9c95c507e8d9e5
Siddharth <sahusidd715@gmail.com>
2024-07-30 09:31:53 +0530

    new file:   INfo about http requests 	modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-let display_text = "";
-let display_area = document.querySelector(".display");
-let buttons = document.querySelectorAll(".btn");
+//Http Methods
+//get : to get some some data from server 
+//post - send data and mutate data in server
+// const fs=require('fs')
+// fs.writeFile("INfo about http requests","get = to some data from the server \n and we use post to send some data to muate in the server ",(err)=>{
+//     if(!err){
+//         console.log("Sucessfully Written!!!")
+//     }
+// })
+const http = require("http");
+// const { url } = require("inspector");
 
-buttons.forEach(button=>{
-    button.addEventListener("click",()=>{
-        if(button.innerText=="C"){
-            display_text="";
-            display_area.innerText=display_text
+const myserver = http.createServer((req, res) => {
+    switch(req.url){
+        case "/":
+            res.end("You are ata home page");
+            break
+        case "/about":
+            res.end("You are at /about page")
+            break;
+    }
+    // res.end("Welcome, you are now connected to the server");
+});
 
-        }
-        else if(button.innerText!="="){
-        display_text+=button.innerText
-        display_area.innerText=display_text
-        }
-        else if(button.innerText=="="){
-            display_area.innerText=eval(display_text)
-        }
-       
-    })
-})
+// Specify the port you want the server to listen on
+const port = 3000;
+myserver.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
